9666a746aba9a04fbcf28b1cbec881a7d9041223
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-27 13:49:56 -0300

    Primeiro commit

---
+++ projeto/src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import './header.scss'
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+export default function Header(){
+    return <div className="header-containter">
+        <MenuIcon/>
+
+    </div>
+}
+
