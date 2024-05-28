9dbb5b42ddc64de4d81c6017d1a1a48955ccb3f2
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-28 11:35:03 -0300

    adicionado info do filme em destaque

--- projeto/src/pages/home/home.tsx
+++ projeto/src/pages/home/home.tsx
@@ -4,7 +4,10 @@ import "./home.scss"
 export default function Home(){
     return  <div className="containter-home">
         <img className="imgDestaque" src="./images/avengersCapa.svg" alt="imagem da capa do filme dos vingadores" />
-        
+        <div className="infoFilme">
+            <h3>Avengers: Infinity War</h3>
+            <p>2018</p>
+        </div>
         
     </div>
 }
