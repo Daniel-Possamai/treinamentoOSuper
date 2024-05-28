e2ca4428ae518ce5a84e2c6d9928bd17ae5d0de5
Daniel Possamai <daniel.possamai20@gmail.com>
2024-05-28 11:50:06 -0300

    Adicionado icones do filme em destaque

--- projeto/src/pages/home/home.tsx
+++ projeto/src/pages/home/home.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import "./home.scss"
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import StarRoundedIcon from '@mui/icons-material/StarRounded';
+import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
 
 export default function Home(){
     return  <div className="containter-home">
@@ -8,6 +10,11 @@ export default function Home(){
         <div className="infoFilme">
             <h3>Avengers: Infinity War</h3>
             <p>2018</p>
+        </div>
+        <div className="btnsFilme">
+            <StarRoundedIcon/>
+            <FavoriteIcon/>
+            <ShareRoundedIcon/>
         </div>
         
     </div>
