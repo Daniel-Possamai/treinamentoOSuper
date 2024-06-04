671a5e5930001fbdb75edf1771d3954e78c70c7d
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-04 14:14:39 -0300

    Adicionado icon profile ao header e ajustado o display grid

--- projeto/src/components/header/header.tsx
+++ projeto/src/components/header/header.tsx
@@ -1,11 +1,13 @@
 import './header.scss'
 import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 export default function Header(){
     return <div className="header-containter">
-        <MenuIcon/>
+        <MenuIcon className={ 'menuIcon' } />
         <img src="./images/logotest.svg" alt="imagem da logo" />
+        <AccountCircleIcon className={ 'profileIcon' }/>
     </div>
 }
 
