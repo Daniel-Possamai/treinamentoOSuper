6e25eb0f73c2d0ffe8b5f3fa3aff9b3a43dbbc00
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-05 10:49:28 -0300

    criado e adicionado o componente nav a versao desktop

--- projeto/src/components/header/header.tsx
+++ projeto/src/components/header/header.tsx
@@ -1,12 +1,14 @@
 import './header.scss'
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Nav from '../nav/nav';
 
 
 export default function Header(){
     return <div className="header-containter">
-        <MenuIcon className={ 'menuIcon' } />
+        <MenuIcon className={ 'menuIcon hiddenDesk' } />
         <img src="./images/logotest.svg" alt="imagem da logo" />
+        <Nav/>
         <AccountCircleIcon className={ 'profileIcon' }/>
     </div>
 }
