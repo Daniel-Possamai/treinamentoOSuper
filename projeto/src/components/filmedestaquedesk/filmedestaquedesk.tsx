56540a88bccd5c0166ef923a7429c02c0a0b9e21
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-05 14:22:13 -0300

    iniciado filme destacado para desktop

---
+++ projeto/src/components/filmedestaquedesk/filmedestaquedesk.tsx
@@ -0,0 +1,11 @@
+import { Filme } from "../../pages/home/home"
+import "./filmedestaquedesk.scss"
+
+interface propsFilmeDestacado{
+    filme: Filme[]
+}
+
+export default function Filmedestaquedesk(props: propsFilmeDestacado){
+    const { filme } = props
+    
+}
