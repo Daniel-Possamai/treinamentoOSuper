8d729b217d5d46b7f813e24c48d8cd24d0185f6b
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-06 13:50:10 -0300

    Adiciona logica para quando a tela for menor que 700px aparecer somente dois filmes e quando for maior que 700px aparecer 4 filmes por topico, foi usado estados no react para definir o numero de filmes mostrado e um escutador de evento na janela para saber quando a tela muda de tamanha, a função é chamada toda vez que a pagina é recarregada e também é removida após ser escutada para não ter vazamento de memoria

--- projeto/src/components/containerfilmes/containerfilmes.tsx
+++ projeto/src/components/containerfilmes/containerfilmes.tsx
@@ -1,5 +1,7 @@
 import "./containerfilmes.scss"
 import { Filme } from "../../pages/home/home"
+import React, { useState, useEffect } from 'react';
+ 
 
 interface propsContainerFilmes {
     filmes: Filme[]
@@ -8,10 +10,30 @@ interface propsContainerFilmes {
 
 export default function Containerfilmes(props: propsContainerFilmes){
     const { filmes, title } = props 
+    const [ numFilmes, setNumFilmes ] = useState(2)
+
+    useEffect(() => {
+        function handleResize(){
+            if (window.matchMedia("(min-width: 700px)").matches){
+                setNumFilmes(4)
+            }else{
+                setNumFilmes(2)
+            }
+        }
+
+        window.addEventListener('resize', handleResize)
+        handleResize()
+
+        return () => window.removeEventListener('resize', handleResize)
+
+    })
+
+
+
     return (<div className={ 'containerFilmes' }>
         <h2>{ title }</h2>
         <div className="organizadorFilmes">
-            { filmes.slice(0,2).map((filme, index) => 
+            { filmes.slice(0, numFilmes).map((filme, index) => 
                 <div key={index} className={ 'filmes' }>
                     <img src={ filme.img } alt= {`Imagem da capa do filme ${filme.title}`} />
                 </div>)} 
