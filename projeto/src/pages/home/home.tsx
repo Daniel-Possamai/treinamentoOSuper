ae6f961e56f68c3dd7c589ce83967328b1e0e297
Daniel Possamai <daniel.possamai20@gmail.com>
2024-06-10 11:15:58 -0300

    adicionado loader ao carregar pagina

--- projeto/src/pages/home/home.tsx
+++ projeto/src/pages/home/home.tsx
@@ -3,7 +3,9 @@ import "./home.scss"
 import Btnplay from "../../components/btnplay/btnplay";
 import Containerfilmes from "../../components/containerfilmes/containerfilmes";
 import Destaquefilme from "../../components/destaquefilme/destaquefilme";
+import Loaderall from "../../components/loaderall/loaderall";
 import { action, forYouFilmes, trendingFilmes } from "../../utils/filmes";
+import { Fragment, useEffect, useState } from "react";
 
 export interface Filme{
     title: string
@@ -14,9 +16,22 @@ export interface Filme{
 
 
 export default function Home(){
+
+    const [ isLoad, setLoad ] = useState(true)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoad(false); 
+        }, 2000);
+    
+        return () => clearTimeout(timer);
+    }, []);
+
     return  <div className={ 'containter-home' }>
+        { isLoad ?  <Loaderall/> : <Fragment>
         <Destaquefilme filmes={ trendingFilmes }/>
         <Btnplay/>
+        
         
         
         <div className={ 'line1' }></div>
@@ -24,6 +39,7 @@ export default function Home(){
         <Containerfilmes filmes={ trendingFilmes } title={ 'Trending' }/>
         <Containerfilmes filmes={ forYouFilmes } title={ 'For You' }/>
         <Containerfilmes filmes={ action } title={ 'Action' }/>
-
+        </Fragment>}
+        
     </div>
 }
